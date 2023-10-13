cf22bd6b26d32461f640e7043b54464fc4894476
rajesh <prajeshyadav64@gmail.com>
2023-10-13 13:36:16 +0530

    This is calculator app

---
+++ IdeaProjects/calculator/src/SimpleCalculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Available Operations:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+        System.out.print("Enter the operation (1/2/3/4): ");
+        int operation = input.nextInt();
+
+        System.out.print("Enter the first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = input.nextDouble();
+
+        double result = 0.0;
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case 2:
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case 3:
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation. Please choose 1, 2, 3, or 4.");
+        }
+
+        input.close();
+    }
+}
